26f333184b87945952c8a10ad96bd701420176e9
len.liu <len.liu@miotech.com>
2023-07-21 11:51:30 +0800

    新base

--- packages/mio/src/App.tsx
+++ packages/mio/src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -13,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-         base Learn React
+          Learn React
         </a>
       </header>
     </div>
